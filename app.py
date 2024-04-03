e86c06decb7f849593a0810c5bab8b2335ff5cb7
MJ <mellejorna@gmail.com>
2024-04-03 12:46:01 +0200

    test

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ import os
 import openai
 from langchain.llms import ChatOpenAI
 from langchain.chains import ChatPromptTemplate, StrOutputParser
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains import AnalyzeDocumentChain
+from langchain_community.callbacks import get_openai_callback
+from langchain.chains.question_answering import load_qa_chain
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from fuzzywuzzy import process
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
