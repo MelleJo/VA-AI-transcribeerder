ae847e29331f8c9c497d6148acdff179cd80af72
MelleJo <mellejorna@gmail.com>
2024-06-17 11:11:57 +0200

    test

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ import pytz
 import tempfile
 from datetime import datetime
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.chains import AnalyzeDocumentChain
-from langchain_community.callbacks import get_openai_callback
-from langchain.chains.question_answering import load_qa_chain
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
