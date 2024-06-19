862acb879ddbb61168fb2d22709a4866271ead18
MelleJo <mellejorna@gmail.com>
2024-06-19 21:35:01 +0200

    test

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def copy_to_clipboard(transcript, summary):
     st.success("Transcript and summary copied to clipboard!")
 
 def main():
-    st.title("Gesprekssamenvatter - testversie 0.1.8.")
+    st.title("Inspreekfunctie werkt tijdelijk niet - ik werk er aan")
 
     with st.sidebar:
         department = st.selectbox("Kies je afdeling", ["Bedrijven", "Financieel Advies", "Schadeafdeling", "Algemeen", "Arbo", "Algemene samenvatting", "Ondersteuning Bedrijfsarts", "Onderhoudsadviesgesprek in tabelvorm", "Notulen van een vergadering", "Verslag van een telefoongesprek", "Deelnemersgesprekken collectief pensioen", "test-prompt (alleen voor Melle!)"])
