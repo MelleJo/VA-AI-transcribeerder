32416be577b3ead3c1dbccd1a48011bf04a33c7e
MelleJo <mellejorna@gmail.com>
2024-06-20 19:11:42 +0200

    test

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ from docx import Document
 from pydub import AudioSegment
 import pytz
 import pyperclip
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains import AnalyzeDocumentChain
+from langchain_community.callbacks import get_openai_callback
+from langchain.chains.question_answering import load_qa_chain
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from fuzzywuzzy import process
 
 PROMPTS_DIR = os.path.abspath("prompts")
 QUESTIONS_DIR = os.path.abspath("questions")
