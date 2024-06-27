69a49f93d51fe6c555d5777c3331bee642b91f17
MelleJo <mellejorna@gmail.com>
2024-06-27 15:57:59 +0200

    test

--- .src/app.py
+++ .src/app.py
@@ -1,14 +1,7 @@
 import os
 import sys
-
-# Add the .src directory to the Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-sys.path.insert(0, current_dir)
-
 import streamlit as st
-from services.openai_service import perform_gpt4_operation
+from openai_service import perform_gpt4_operation
 from utils.audio_processing import transcribe_audio, process_audio_input
 from utils.file_processing import process_uploaded_file
 from services.summarization_service import summarize_text
