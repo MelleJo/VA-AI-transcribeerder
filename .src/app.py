f8b9524f3e921c25627363deb85a005b113cffda
MelleJo <mellejorna@gmail.com>
2024-06-27 15:54:45 +0200

    test

--- .src/app.py
+++ .src/app.py
@@ -1,12 +1,17 @@
 import os
+import sys
+
+# Add the project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
-from utils.audio_processing import transcribe_audio, process_audio_input
-from utils.file_processing import process_uploaded_file
-from services.summarization_service import summarize_text
-from ui.components import setup_page_style, display_transcript, display_summary
-from ui.pages import render_feedback_form, render_conversation_history
-from services.openai_service import perform_gpt4_operation
-from utils.text_processing import update_gesprekslog, copy_to_clipboard, load_questions
+from src.utils.audio_processing import transcribe_audio, process_audio_input
+from src.utils.file_processing import process_uploaded_file
+from src.services.summarization_service import summarize_text
+from src.ui.components import setup_page_style, display_transcript, display_summary
+from src.ui.pages import render_feedback_form, render_conversation_history
+from src.services.openai_service import perform_gpt4_operation
+from src.utils.text_processing import update_gesprekslog, copy_to_clipboard, load_questions
 
 
 
