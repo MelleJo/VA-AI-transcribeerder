6a7ce0dad0eaa9fa54ff5703165436b18d65e324
MelleJo <mellejorna@gmail.com>
2024-08-29 12:01:08 +0200

    test

--- .src/app.py
+++ .src/app.py
@@ -2,9 +2,14 @@ import streamlit as st
 import os
 import sys
 import json
-from .src.ui.pages import render_wizard, render_feedback_form, render_conversation_history
-from .src.ui.components import setup_page_style, initialize_session_state
-
+from openai_service import perform_gpt4_operation
+from utils.audio_processing import process_audio_input
+from services.summarization_service import run_summarization
+from utils.file_processing import process_uploaded_file
+from ui.pages import render_wizard, render_feedback_form, render_conversation_history
+from ui.components import setup_page_style, initialize_session_state
+from utils.text_processing import update_gesprekslog, load_questions
+from docx import Document
 from io import BytesIO
 import bleach
 import base64
