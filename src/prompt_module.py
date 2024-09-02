04ec5a4ad9e75474e09d34f79781dc88909f3218
MelleJo <mellejorna@gmail.com>
2024-09-02 21:14:40 +0200

    test

--- src/prompt_module.py
+++ src/prompt_module.py
@@ -6,8 +6,8 @@ from src import utils
 def render_prompt_selection():
     st.header("Stap 1: Selecteer instructies")
     
-    prompts = utils.load_prompts()
-    selected_prompt = st.selectbox("Kies een instructieset:", list(prompts.keys()))
+    prompts = utils.get_prompt_names()  # This now excludes the base prompt
+    selected_prompt = st.selectbox("Kies een instructieset:", prompts)
     
     if selected_prompt:
-        st.session_state.selected_prompt = prompts[selected_prompt]
+        st.session_state.selected_prompt = selected_prompt
