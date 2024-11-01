d43c39f9d18fc340382ab48c10a04e8407e2f844
Melle <mellejorna@gmail.com>
2024-11-01 12:35:01 +0100

    fix imports

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ st.set_page_config(page_title="Gesprekssamenvatter AI - testversie 0.0.2", layou
 from src import config, prompt_module, input_module, summary_and_output_module, ui_components, history_module
 from src.utils import post_process_grammar_check, format_currency, load_prompts, get_prompt_names, get_prompt_content
 from src.state_utils import convert_summaries_to_dict_format
+# Add this line to import add_loader_css specifically:
+from src.ui_components import add_loader_css, apply_custom_css, full_screen_loader
 import logging
 import time
 import os
@@ -90,9 +92,9 @@ def load_css():
 
 def main():
     load_css()
-    add_loader_css()
+    add_loader_css()  # Now this will be properly defined
     ui_components.apply_custom_css()
-    convert_summaries_to_dict_format()
+    convert_summaries_to_dict_format(st)  # Pass st as argument
 
     # Initialize OpenAI client using the function to get the API key
     client = OpenAI(api_key=config.get_openai_api_key())
